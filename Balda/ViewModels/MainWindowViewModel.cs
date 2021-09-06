2ca12a58397de58f6fdd8be85350e8598d47260c
karisDev <karjaakirill@gmail.com>
2021-09-06 23:31:20 +0300

    MVVM Improvements and UI changes

--- Balda/ViewModels/MainWindowViewModel.cs
+++ Balda/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Balda.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace Balda.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        private string _Title = "Балда";
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+                /*              if (Equals(_Title, value)) return;
+                                _Title = value;
+                                OnPropertyChanged();*/
+            set => Set(ref _Title, value);
+        }
     }
 }
